1a69fb04de509905144aa7774f319ff075463077
theonlyrehman <166296111+theonlyrehman@users.noreply.github.com>
2024-05-29 05:47:13 +0500

    push my word counter on github

---
+++ index.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
+]);
+const word = answer.sentence.trim().split(" ");
+console.log(word);
+console.log(`Your sentence word count is ${word.length}`);
---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answer: {
+    sentence:string 
+} = await inquirer.prompt (
+    [
+        {
+            name: "sentence",
+            type: "input",
+            message: "Enter your sentence to count the word: "
+        }
+    ]
+);
+
+const word = answer.sentence.trim().split(" ")
+
+console.log(word);
+
+console.log(`Your sentence word count is ${word.length}`);
